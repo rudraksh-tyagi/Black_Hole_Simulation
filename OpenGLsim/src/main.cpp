96e1294f79ec23d714ccec9bb42b7e04d77f6f0b
RudraDSA <tyagirudraksh.bij@gmail.com>
2026-01-19 18:54:12 +0530

    open gl working on vs code cpp

---
+++ OpenGLsim/src/main.cpp
@@ -0,0 +1,37 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "GLFW init failed\n";
+        return -1;
+    }
+
+    GLFWwindow* window =
+        glfwCreateWindow(800, 600, "OpenGL Works", nullptr, nullptr);
+
+    if (!window) {
+        std::cerr << "Window creation failed\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "GLAD init failed\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
